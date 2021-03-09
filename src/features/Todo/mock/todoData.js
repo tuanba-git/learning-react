2e64e35b1ab4e6575fe97688182d47154349097c
Bui Tuan <buianhtuan657@gmail.com>
2021-03-09 19:33:26 +0700

    move mock data to folder Mockdata

---
+++ src/features/Todo/mock/todoData.js
@@ -0,0 +1,60 @@
+const todoList = [
+  {
+    id: '01',
+    title: 'Working',
+    description:
+      '1 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum auctor tempor sapien',
+    status: 'doing'
+  },
+  {
+    id: '02',
+    title: 'Study',
+    description:
+      '2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum auctor tempor sapien',
+    status: 'doing'
+  },
+  {
+    id: '03',
+    title: 'Play',
+    description:
+      '3 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum auctor tempor sapien',
+    status: 'doing'
+  },
+  {
+    id: '04',
+    title: 'Coding',
+    description:
+      '4 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum auctor tempor sapien',
+    status: 'doing'
+  },
+  {
+    id: '05',
+    title: 'Running',
+    description:
+      '1 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum auctor tempor sapien',
+    status: 'doing'
+  },
+  {
+    id: '06',
+    title: 'Read Book',
+    description:
+      '2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum auctor tempor sapien',
+    status: 'done'
+  },
+  {
+    id: '07',
+    title: 'Listen Music',
+    description:
+      '3 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum auctor tempor sapien',
+    status: 'doing'
+  },
+  {
+    id: '08',
+    title: 'Planting',
+    description:
+      '4 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum auctor tempor sapien',
+    status: 'done'
+  }
+];
+
+export default todoList;
